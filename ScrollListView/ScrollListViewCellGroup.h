fdcde9e7099852dba8d613ec99a4636795ee56c5
Henry <zhy289044312@gmail.com>
2015-11-11 00:06:12 +0800

    增加关于Cell的管理类group的内部数据结构 增加关于Cell的管理类group的相关操作方法

--- ScrollListView/ScrollListViewCellGroup.h
+++ ScrollListView/ScrollListViewCellGroup.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+@class ScrollListCellView;
+
 @interface ScrollListViewCellGroup : NSObject
 
 - (id)initWithIdentifer:(NSString *)identifier;
@@ -18,3 +20,29 @@
 
 @end
 
+/**
+ * Define Basic Action Method For Class Group
+ */
+@interface ScrollListViewCellGroup (Extend)
+
+/**
+ * After ScrollListView delegate method 'scrollListView:cellForViewAtIndexPath:' , this method need to call for reuse cell by add in group with identifier.
+ */
+- (void)addCell:(id)cell;
+
+/**
+ * In general,this method need not call directly.
+ */
+- (void)removeCell:(id)cell;
+
+/**
+ * This method will remove all cells' Ref in group.
+ */
+- (void)resetGroup;
+
+/**
+ * Request a cell if prepared for reuse. Return nil when no cells prepared.
+ */
+- (ScrollListCellView *)preparedForReuseCell;
+
+@end
--- ScrollListView/ScrollListViewCellGroup.m
+++ ScrollListView/ScrollListViewCellGroup.m
@@ -7,10 +7,13 @@
 //
 
 #import "ScrollListViewCellGroup.h"
+#import "ScrollListCellView.h"
 
 @interface ScrollListViewCellGroup ()
 
-@property (nonatomic, strong) NSMutableArray *mCells;
+@property (nonatomic, strong) NSMutableArray *mCells;       //Contain all cells with same identifier.
+@property (nonatomic, strong) NSMutableArray *mUsingCells;  //Contain 'USING' Cells
+@property (nonatomic, strong) NSMutableArray *mReuseCells;  //Contain 'REUSE' Cells
 
 @end
 
@@ -24,6 +27,49 @@
         _identifier = identifier;
     }
     return self;
+}
+
+#pragma mark - Public Property
+
+- (NSArray *)cells {
+    if (self.mCells.count == 0) {
+        return nil;
+    }
+    return [NSArray arrayWithArray:self.mCells];
+}
+
+#pragma mark - Private Property
+
+- (NSMutableArray *)mCells {
+    if (!_mCells) {
+        _mCells = [NSMutableArray array];
+    }
+    return _mCells;
+}
+
+@end
+
+@implementation ScrollListViewCellGroup (Extend)
+
+- (void)addCell:(id)cell {
+   
+}
+
+- (void)removeCell:(id)cell {
+    
+}
+
+- (void)resetGroup {
+    
+}
+
+- (ScrollListCellView *)preparedForReuseCell {
+    if (self.mReuseCells.count == 0) {
+        return nil;
+    }
+    ScrollListCellView *tCell = self.mReuseCells.firstObject;
+    [tCell prepareForReuse];
+    return tCell;
 }
 
 @end
